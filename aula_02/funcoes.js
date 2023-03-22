1980945f954d75335aed448d439c6a72bb07e09f
Batstinha <127945660+Batstinha@users.noreply.github.com>
2023-03-22 08:31:24 -0300

    aula_02

---
+++ aula_02/funcoes.js
@@ -0,0 +1,30 @@
+// let num1 = 2
+// const num2 = 3
+// var msg = "Olá Meus amigus, Manoel Gomi aqui"
+
+// console.log(msg , num1)
+// console.log(num1 + num2)
+// console.log(typeof num1)
+// console.log(typeof msg)
+
+// JSON - JavaScript Object Notation:
+
+// let usuario = {
+//     nome: "Adriel",
+//     idade: 16,
+//     signo: "Áries",
+//     calvo: true
+// }
+ 
+// console.log(typeof usuario)
+// console.log(usuario)
+
+// console.log("nome:", usuario.nome, "\n Idade:", usuario.idade)
+// console.log(typeof usuario.calvo)
+
+const nomes = ["João", "Oãoj"]
+
+console.log(nomes)
+console.log(typeof nomes)
+
+console.log(nomes[0])
